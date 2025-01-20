1490300810c4f8bb446db64a730e16e1393dde5a
Jose Alberto Chaparro Castro <jachaparro@unicesar.edu.co>
2025-01-20 18:30:59 -0500

    test 2

--- configuracion/firebase_config.py
+++ configuracion/firebase_config.py
@@ -10,21 +10,12 @@ load_dotenv()
 def inicializar_firebase():
     if not firebase_admin._apps:
         try:
-            # Usar directamente el JSON de credenciales en lugar del archivo
-            cred = credentials.Certificate({
-                "type": "service_account",
-                "project_id": "hackaton-b96e0",
-                "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
-                "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
-                "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
-                "client_id": os.getenv('FIREBASE_CLIENT_ID'),
-                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-                "token_uri": "https://oauth2.googleapis.com/token",
-                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-                "client_x509_cert_url": os.getenv('FIREBASE_CLIENT_X509_CERT_URL')
-            })
+            cred_path = os.getenv('configuracion/hackaton-b96e0-firebase-adminsdk-mcv6w-d484e4c384.json')
+            cred = credentials.Certificate(cred_path)
             firebase_admin.initialize_app(cred)
-            return firestore.client()
+            db = firestore.client()
+            print("Conexión con Firebase establecida correctamente")
+            return db
         except Exception as e:
             print(f"Error al conectar con Firebase: {str(e)}")
             raise e
